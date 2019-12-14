a2f3cccd7a60a709275a26cc5d8c9f288e2d7d59
Andrew <andrew@cowboycoder.com>
2019-12-14 13:27:02 +0000

    Useful methods for working out screen positions for layouts and widgets

--- sdk/src/main/java/net/activelook/sdk/util/Point.kt
+++ sdk/src/main/java/net/activelook/sdk/util/Point.kt
@@ -1,6 +1,17 @@
 package net.activelook.sdk.util
 
+import net.activelook.sdk.screen.Screen
+
 /**
  * Coordinates of a point
  */
-data class Point(val x: Int, val y: Int)
+data class Point(val x: Int, val y: Int) {
+
+    fun offset(padX: Int, padY: Int) : Point {
+        return Point(x + padX, y + padY)
+    }
+
+    fun invert() : Point {
+        return Point(Screen.WIDTH - x, Screen.HEIGHT - y)
+    }
+}
